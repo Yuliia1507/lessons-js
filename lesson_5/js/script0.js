d556a7d4831d5182e9964978403b407fac9b46b1
Yuliia <144613150+Yuliia1507@users.noreply.github.com>
2025-08-06 17:42:31 +0300

    Delete lesson_5 directory

--- lesson_5/js/script0.js
+++
@@ -1,6 +0,0 @@
-
-// Вивести на екран 8 кнопок з написом “Hello”
-
-for (let buttonNum = 1; buttonNum <= 8; buttonNum++) {
-	document.write(`<button class="hellobtn">Hello ${buttonNum}</button><br>`)
-}

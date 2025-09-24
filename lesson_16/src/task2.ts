9d3ab412441c674b5dca977de42d76d03f48792e
Yuliia <144613150+Yuliia1507@users.noreply.github.com>
2025-09-24 11:49:35 +0300

    Add files via upload

--- lesson_16/src/task2.ts
+++ lesson_16/src/task2.ts
@@ -1,12 +1,12 @@
 // Задача 2. Створити клас TMoney для роботи з грошовими сумами.Сума повинна зберігатися у вигляді доларового еквіваленту.Реалізувати методи додавання / вилучення грошової маси, вказуючи необхідну суму у гривнях, та визначення курсу долара, при якому сума у гривнях збільшиться на 100. Курс долара зберігати в окремому полі.
 
 class TMoney {
-	private _dollarsSum: number; // сума в доларах
-	private _rate: number;       //  курс долара 
+	private _dollarsSum: number = 0; // сума в доларах
+	private _rate: number = 0;       // курс долара 
 
 	constructor(initialDollars: number, rate: number) {
-		this._dollarsSum = initialDollars;
-		this._rate = rate;
+		this.setDollars(initialDollars);
+		this.setRate(rate);
 	}
 
 	public get Dollars(): number {
@@ -23,6 +23,11 @@ class TMoney {
 		this._rate = newRate;
 	}
 
+	// приватний метод для встановлення суми доларів
+	private setDollars(amount: number) {
+		if (amount < 0) throw new Error("Сума не може бути від'ємною");
+		this._dollarsSum = amount;
+	}
 
 	//  додати суму в гривнях 
 	public addUAH(amountUAH: number) {
@@ -38,7 +43,6 @@ class TMoney {
 		this._dollarsSum -= dollarsToSubtract;
 	}
 
-
 	public rateForIncreaseUAH(increaseUAH: number): number {
 		if (increaseUAH <= 0) throw new Error("Невірна сума");
 		return (this._dollarsSum * this._rate + increaseUAH) / this._dollarsSum;
@@ -50,22 +54,20 @@ class TMoney {
 	}
 }
 
-//  Тестування 
+// Тестування
 try {
-	const money = new TMoney(100, 40); // 100$, курс 40 грн/$
+	const money = new TMoney(100, 40);
 	document.write(money.toString() + '<br>');
 
-	money.addUAH(800); // додаємо 800 грн
+	money.addUAH(800);
 	document.write(money.toString() + '<br>');
 
-	money.subtractUAH(400); // вилучаємо 400 грн
+	money.subtractUAH(400);
 	document.write(money.toString() + '<br>');
 
-	const newRate = money.rateForIncreaseUAH(100); // курс, щоб грн збільшились на 100
+	const newRate = money.rateForIncreaseUAH(100);
 	document.write(`Новий курс для +100 грн: ${newRate.toFixed(2)}<br>`);
-
 } catch (err: any) {
 	document.write("Помилка: " + err.message);
 }
-
 
